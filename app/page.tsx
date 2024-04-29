ca8df49c1d1b3eea51b2e480260cbd3aa7c72206
Bhargav Vivek Modak <thebrahmnicboy@gmail.com>
2024-04-29 16:28:37 +0530

    feat: Add buttons to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,19 @@
 "use client";
-import { useTime, useTransform } from "framer-motion";
 import React from "react";
 import { LandingPage } from "@/components/landing-page";
 
 export default function Home() {
-  const ref = React.useRef(null);
-  const time = useTime();
+    const ref = React.useRef(null);
 
-  const pathLengthFirst = useTransform(time, [0, 4000], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(time, [0, 4000], [0.15, 1.2]);
-  const pathLengthThird = useTransform(time, [0, 4000], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(time, [0, 4000], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(time, [0, 4000], [0, 1.2]);
-
-  return (
-    <div
-      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-      ref={ref}
-    >
-      <LandingPage
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-        title="SnapTunes"
-        description="Transform pixels into music."
-      />
-    </div>
-  );
+    return (
+        <div
+            className="h-screen bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-28 overflow-clip"
+            ref={ref}
+        >
+            <LandingPage
+                title="SnapTunes"
+                description="Transform pixels into music."
+            />
+        </div>
+    );
 }
