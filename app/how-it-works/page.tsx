d8d0988f2059b3f9dba622f89d4a6f3b12409a37
Bhargav Vivek Modak <thebrahmnicboy@gmail.com>
2024-05-03 00:06:09 +0530

    feat: Add content for how-it-works

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -38,25 +38,42 @@ const HowItWorks = () => {
 
 export default HowItWorks;
 
+const CustomH3 = ({ children }: { children: React.ReactNode }) => (
+    <h3 className={twMerge(geologica.className, 'text-2xl my-2 text-blue-200')}>
+        ### {children}
+    </h3>
+);
+
+const CustomA = ({ href, children }: { href: string; children: React.ReactNode }) => (
+    <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-500 hover:underline"
+    >
+        {children}
+    </a>
+);
+
 const dummyContent = [
     {
         title: 'Gemini 1.0 Pro',
         description: (
             <>
-                <h3>What is Gemini 1.0 Pro?</h3>
+                <CustomH3>What is Gemini 1.0 Pro?</CustomH3>
                 <p>
                     Gemini 1.0 Pro is the LLM (Large Language Model) that is provided by
                     Google to help developers build better applications. It is a powerful
                     tool that can be used to generate text, images, and code.
                 </p>
-                <h3>How does it work?</h3>
+                <CustomH3>How does it work?</CustomH3>
                 <p>
                     Gemini 1.0 Pro works by using a neural network to generate text based
                     on the input that it receives. The model is trained on a large dataset
                     of text, which allows it to generate text that is similar to the input
                     that it receives.
                 </p>
-                <h3>What can it be used for?</h3>
+                <CustomH3>What can it be used for?</CustomH3>
                 <p>
                     Gemini 1.0 Pro can be used for a wide range of applications, including
                     generating text for chatbots, generating code for software
@@ -67,83 +84,112 @@ const dummyContent = [
         ),
     },
     {
-        title: 'Connection with Spotify',
+        title: 'Using the Gemini 1.0 Pro API',
         description: (
             <>
+                <CustomH3>What is the Gemini 1.0 Pro API?</CustomH3>
                 <p>
-                    Sit duis est minim proident non nisi velit non consectetur. Esse
-                    adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-                    Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-                    incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-                    fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-                    nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-                    occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-                    officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-                    commodo et labore dolore commodo pariatur.
+                    The Gemini 1.0 Pro API is a robust interface that allows developers to
+                    interact with Google's advanced LLM. It supports a variety of
+                    operations, including text generation, image analysis, and code
+                    synthesis. The API accepts JSON-formatted requests and returns
+                    responses in the same format, facilitating seamless integration into
+                    web and mobile applications.
                 </p>
                 <p>
-                    Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-                    veniam in commodo id reprehenderit adipisicing. Proident duis
-                    exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
+                    You can read more about the Gemini 1.0 Pro API in the
+                    <CustomA href="https://ai.google.dev/gemini-api/docs/models/gemini">
+                        {' '}
+                        official documentation
+                    </CustomA>
+                    .
                 </p>
+                <CustomH3>How to use the Gemini 1.0 Pro API</CustomH3>
                 <p>
-                    Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
-                    reprehenderit deserunt amet laborum consequat adipisicing officia qui
-                    irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-                    Amet culpa officia aliquip deserunt veniam deserunt officia
-                    adipisicing aliquip proident officia sunt.
+                    To prompt the LLM, we construct a JSON payload that includes the
+                    necessary parameters and data. For SnapTunes, we use a custom system
+                    message that specifies the input as an image, and the output as a JSON
+                    string. This message is encoded and sent to the API, which then
+                    processes the image using its neural network to understand the content
+                    and context, ultimately suggesting songs that align with the image's
+                    vibe.
+                </p>
+                <CustomH3>Interpreting the Gemini 1.0 Pro response</CustomH3>
+                <p>
+                    Once the LLM processes the input, it generates a response that
+                    includes potential song suggestions as a JSON string. Our system
+                    parses this output, then we can use the names of the songs to query
+                    the Spotify API for more information.
                 </p>
             </>
         ),
     },
     {
-        title: 'The Landing Page Animations',
+        title: 'The Animations',
         description: (
             <>
+                <CustomH3>Aceternity UI</CustomH3>
                 <p>
-                    Sit duis est minim proident non nisi velit non consectetur. Esse
-                    adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-                    Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-                    incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-                    fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-                    nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-                    occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-                    officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-                    commodo et labore dolore commodo pariatur.
+                    <CustomA href="https://ui.aceternity.com/">Aceternity UI</CustomA> is
+                    a treasure trove of interactive React components, offering a wide
+                    array of micro-interactions and animations for web design. Built with
+                    Next.js, Framer Motion, and Tailwind CSS, it provides developers with
+                    a plethora of animated components that are ready to copy and paste
+                    into your projects.
                 </p>
+                <CustomH3>Framer Motion</CustomH3>
                 <p>
-                    Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-                    veniam in commodo id reprehenderit adipisicing. Proident duis
-                    exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
+                    <CustomA href="https://www.framer.com/motion/">Framer Motion</CustomA>{' '}
+                    is a production-ready motion library for React that simplifies
+                    creating complex animations. It offers a declarative syntax for
+                    animation, making your codebase easier to read and maintain. With
+                    Framer Motion, you can animate between different components smoothly
+                    and handle complex events and gestures.
                 </p>
+                <CustomH3>SVGs and Icons</CustomH3>
                 <p>
-                    Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
-                    reprehenderit deserunt amet laborum consequat adipisicing officia qui
-                    irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-                    Amet culpa officia aliquip deserunt veniam deserunt officia
-                    adipisicing aliquip proident officia sunt.
+                    For SVGs, we utilize resources like SVGrepo, which provides over
+                    500,000 open-licensed SVG vectors and icons for commercial use without
+                    any royalties. Font Awesome is another source, offering a vast library
+                    of icons and components that are easily customizable for web projects.
+                    Additionally, unDraw.co offers open-source illustrations that can be
+                    used freely in any project, providing beautiful SVG images that can be
+                    customized to match your brand.
                 </p>
             </>
         ),
     },
     {
-        title: 'Other Animations',
+        title: 'Spotify',
         description: (
             <>
+                <CustomH3>What is the Spotify API?</CustomH3>
                 <p>
-                    Sit duis est minim proident non nisi velit non consectetur. Esse
-                    adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-                    Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-                    incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-                    fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-                    nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-                    occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-                    officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-                    commodo et labore dolore commodo pariatur.
+                    The Spotify API provides developers with programmatic access to the
+                    vast Spotify music catalog and user data. It allows for operations
+                    such as searching the Spotify library, managing playlists, and playing
+                    music. Our application utilizes this API to enhance the user
+                    experience by connecting them with music that resonates with the
+                    emotions and themes captured in their photographs.
                 </p>
+                <CustomH3>Searching for songs</CustomH3>
                 <p>
-                    Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-                    veniam in commodo id repreh
+                    Fuzzy searching is a technique that finds matches even when the search
+                    terms are not exact, which is crucial for interpreting the varied
+                    inputs from users. Our system implements fuzzy searching through the
+                    Spotify API to retrieve song IDs that best match the mood and elements
+                    of the user's image. This process requires a valid Spotify token,
+                    which we regenerate every 1800000 milliseconds (30 minutes) to
+                    maintain uninterrupted service.
+                </p>
+                <CustomH3>Embedding Spotify song iframes</CustomH3>
+                <p>
+                    Once we have the song IDs, we can embed Spotify's song iframes into
+                    our application, allowing users to play previews of the suggested
+                    songs directly on our platform. This feature enhances interactivity
+                    and engagement, providing users with an immediate and seamless
+                    audio-visual experience. The embedding of song iframes does not
+                    require a token, making it a straightforward process.
                 </p>
             </>
         ),
