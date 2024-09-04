3fc101fcd3f0cb08c94faf1bdd6901ced8a4ec33
Bhargav Vivek Modak <bhargav0modak@gmail.com>
2024-09-05 00:14:17 +0530

    fix: remove landing page animations and simplify design
    - prepare landing page for future improvements
    - remove aceternity animations from landing page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import dynamic from 'next/dynamic';
-const AnimatedPaths = dynamic(() => import('./landing-page/animated-paths'), {
-    ssr: false,
-});
+
+import './landing-page/landing-page.css';
 
 import { Geologica, Jersey_25 } from 'next/font/google';
 
@@ -18,34 +16,39 @@ const LandingPage = ({
     title?: string;
     description?: string;
 }) => {
+    const handleHoverEffect = (e: React.MouseEvent<HTMLDivElement>) => {
+        const { currentTarget: target } = e;
+
+        const rect = target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        target.style.setProperty('--mouse-x', `${x}px`);
+        target.style.setProperty('--mouse-y', `${y}px`);
+        // color based on mouse position
+        const randomR = (x / rect.width) * 255;
+        const randomG = (y / rect.height) * 255;
+        const randomB = ((x + y) / (rect.width + rect.height)) * 255;
+        target.style.setProperty(
+            '--random-color',
+            `rgba(${randomR}, ${randomG}, ${randomB}, 0.25)`
+        );
+    };
+
     return (
-        <div className="w-screen">
-            <div className="flex flex-col pt-160 pb-10 items-center">
-                {/* Title */}
-                <p
-                    className={`${geologica.className} text-5xl md:text-7xl pb-4 text-center`}
-                >
+        <div
+            className="flex flex-row flex-wrap-reverse h-screen hover-parent"
+            onMouseMove={handleHoverEffect}
+        >
+            <div className="w-full lg:w-1/2 h-1/2 lg:h-screen flex flex-col justify-center items-center">
+                <p className={`${geologica.className} text-5xl md:text-7xl pb-4`}>
                     {title}
                 </p>
-                {/* Tagline */}
                 <p
-                    className={`${jersey25.className} text-lg md:text-2xl font-normal text-center text-neutral-400 my-4 max-w-lg mx-auto`}
+                    className={`${jersey25.className} text-lg md:text-2xl text-neutral-400 my-4 max-w-lg `}
                 >
                     {description}
                 </p>
-                {/* Icon/Logo */}
-                <div className="mb-10 md:mb-20">
-                    {/* Pill */}
-                    <div
-                        className="w-40 h-40 rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-28 mt-9 z-30"
-                        style={{
-                            backgroundImage: `url('./logo.png')`,
-                            backgroundSize: 'contain',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundPosition: 'center center',
-                        }}
-                    ></div>
-                </div>
                 {/* Two buttons */}
                 <div>
                     <div className="z-50 relative flex">
@@ -56,26 +59,25 @@ const LandingPage = ({
                         </Link>
                         <Link href="/how-it-works">
                             <button className="rounded-full px-3 py-2 ml-4 bg-white text-black hover:bg-blue-600 hover:text-white">
-                                How it Works
+                                How we did it
                             </button>
                         </Link>
                     </div>
                 </div>
             </div>
-            {/* <AnimatedPaths /> */}
-            {/* Footer */}
-            <div className="w-full flex justify-center z-50 items-center absolute bottom-0">
-                <p className="text-xs text-neutral-400">
-                    Inspired by{' '}
-                    <a
-                        href="https://deepmind.google/technologies/gemini/#build-with-gemini"
-                        className="text-blue-400"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        &apos;Build With Gemini&apos;
-                    </a>
-                </p>
+
+            {/* Icon/Logo */}
+            <div className="w-full lg:w-1/2 h-1/2 lg:h-screen flex justify-center items-center">
+                {/* Pill */}
+                <div
+                    className="w-64 h-64 rounded-full z-30"
+                    style={{
+                        backgroundImage: `url('./logo.png')`,
+                        backgroundSize: 'contain',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center center',
+                    }}
+                ></div>
             </div>
         </div>
     );
